a25a86802942717794db6ded54a00edf1b1c4b91
Manan Kakkar <54740965+kakkarmanan@users.noreply.github.com>
2021-10-09 01:03:45 +0530

    meet file change

--- lib/meet.dart
+++ lib/meet.dart
@@ -1,1 +1,16 @@
 import 'package:flutter/material.dart';
+
+class MeetPage extends StatelessWidget {
+  const MeetPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          "Hello"
+        ),
+      ),
+    );
+  }
+}
