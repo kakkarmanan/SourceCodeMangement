7a3d725a0f3e25ef8b8405d57abdd5846f01c2a7
Manan Kakkar <54740965+kakkarmanan@users.noreply.github.com>
2021-10-12 10:54:55 +0530

    new file created

---
+++ lib/new_file.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class new_file extends StatelessWidget {
+  const new_file({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(),
+    );
+  }
+}
